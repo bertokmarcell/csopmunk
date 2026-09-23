7e863426a6cc19aca13a35c50ea4404ab41f9115
Bertók Marcell (SZF_N_2025_2027) <bertok.marcell@diak.szamalk-szalezi.hu>
2026-09-23 16:45:01 +0200

    elso

---
+++ src/keveres/Lapt.java
@@ -0,0 +1,14 @@
+
+package keveres;
+    
+public class Lapt {
+    private String ertek;
+    private String szin;
+    public Lapt(String ertek,String szin) {
+        this.ertek=ertek;
+        this.szin=szin;
+    }
+    
+    
+   
+}
