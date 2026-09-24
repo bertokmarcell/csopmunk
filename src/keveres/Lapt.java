d6a7edc013cbf4e8164002c798f8da2a9f54fb9c
bertok.marcell <bertok.marcell@DESKTOP-7OHRVLU.akk.lan>
2026-09-24 12:47:13 +0200

    starting

--- src/keveres/Lapt.java
+++ src/keveres/Lapt.java
@@ -8,7 +8,18 @@ public class Lapt {
         this.ertek=ertek;
         this.szin=szin;
     }
+
+    public String getErtek() {
+        return ertek;
+    }
+
+    public String getSzin() {
+        return szin;
+    }
     
+    public void kiir(){
+        System.out.print(getErtek()+" "+getSzin());
+    }
     
    
 }
