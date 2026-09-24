1d1472d04f5b1a71fbd0d3fd8e92828da55c7405
bertokmarcell <bertokmarcell@gmail.com>
2026-09-24 19:30:30 +0200

    Hazifeladat2

--- src/Test/Testek.java
+++ src/Test/Testek.java
@@ -13,16 +13,36 @@ public class Testek {
     pakli.keveres(1);
     assert KartyaPakli.getPakli() != null;
     }
-    public static void testnemlehetNull() {
+    public static void testnemlehetNullkeverekor() {
     KartyaPakli pakli = new KartyaPakli();
     pakli.keveres(1);
-    Lap[] lapok = KartyaPakli.getPakli();
-    for (Lap lap : lapok) {
-    assert lap != null;
+    Lap[] lapok=KartyaPakli.getPakli();
+    for (Lap lap:lapok) {
+    assert lap!=null;
     }  
     }
     public static void testkeveres1cseretest(){
-    
-        
+    KartyaPakli pakli = new KartyaPakli();
+    Lap[] testpack = KartyaPakli.getPakli();
+    String test1 = testpack[0].getErtek();
+    String test2 = testpack[0].getSzin();
+    pakli.keveres(1);
+    assert testpack[20].getErtek().equals(test1);
+    assert testpack[20].getSzin().equals(test2);
     }
+    public static void testKeveresLapMegmaradas() {
+    KartyaPakli pakli = new KartyaPakli();
+    Lap[] lapok = KartyaPakli.getPakli();
+    String szin = lapok[0].getSzin();
+    String ertek = lapok[0].getErtek();
+    pakli.keveres(1);
+    boolean megtalalta = false;
+    for (Lap lap : lapok) {
+        if (lap.getSzin().equals(szin) &&
+            lap.getErtek().equals(ertek)) {
+            megtalalta = true;
+        }
+    }
+    assert megtalalta;
+}
 }
