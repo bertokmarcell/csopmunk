5a67f6595fbc0a10180cdae1adca8e6eeea8eb66
bertokmarcell <bertokmarcell@gmail.com>
2026-09-24 19:02:36 +0200

    hazi1

--- src/Test/Testek.java
+++ src/Test/Testek.java
@@ -1,20 +1,28 @@
 package Test;
 
 import keveres.KartyaPakli;
+import keveres.Lap;
 
 public class Testek {
     static KartyaPakli pakli = new KartyaPakli();
     public static void main(String[] args) {
         
     }
-    public static void keveres1(){
-        pakli.feltolt();
-        pakli.keveres(1);
-        boolean works=false;
-        for (int i = 0; i < 21; i++) {
-            if(pakli[i]=null){
-                
-            }
-        }
+    public static void testkeveres1lefut(){
+    KartyaPakli pakli = new KartyaPakli();
+    pakli.keveres(1);
+    assert KartyaPakli.getPakli() != null;
+    }
+    public static void testnemlehetNull() {
+    KartyaPakli pakli = new KartyaPakli();
+    pakli.keveres(1);
+    Lap[] lapok = KartyaPakli.getPakli();
+    for (Lap lap : lapok) {
+    assert lap != null;
+    }  
+    }
+    public static void testkeveres1cseretest(){
+    
+        
     }
 }
