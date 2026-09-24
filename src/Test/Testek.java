00ec57df54c5d5e2399ce4d0dacf7e0c5d6c7d0a
bertok.marcell <bertok.marcell@DESKTOP-7OHRVLU.akk.lan>
2026-09-24 15:40:19 +0200

    teszteles

---
+++ src/Test/Testek.java
@@ -0,0 +1,12 @@
+package Test;
+
+import keveres.KartyaPakli;
+
+public class Testek {
+    public static void main(String[] args) {
+        
+    }
+    public static void keveres1(){
+        
+    }
+}
