279bd4423508a3288c2ffc61de04c7f5e155ed6c
bertok.marcell <bertok.marcell@DESKTOP-7OHRVLU.akk.lan>
2026-09-24 15:53:38 +0200

    teszteles2

--- src/Test/Testek.java
+++ src/Test/Testek.java
@@ -3,10 +3,18 @@ package Test;
 import keveres.KartyaPakli;
 
 public class Testek {
+    static KartyaPakli pakli = new KartyaPakli();
     public static void main(String[] args) {
         
     }
     public static void keveres1(){
-        
+        pakli.feltolt();
+        pakli.keveres(1);
+        boolean works=false;
+        for (int i = 0; i < 21; i++) {
+            if(pakli[i]=null){
+                
+            }
+        }
     }
 }
